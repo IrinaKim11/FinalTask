62d7096fe25fc9fb5f027f9af7903a4f7b3ed341
Irina <i.kim1994@mail.ru>
2022-12-03 17:57:49 +0300

    Изменили решение

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,20 +1,30 @@
-﻿string[] array = new string[6] { "adsdaw", "1", "DVSV", "KokkoI", "hh", "dsgshd"};
+﻿string[] array = new string[6] { "adsdaw", "1", "DVSV", "KokkoI", "hh", "dsgshd" };
 string[] result = new string[array.Length];
 
 void GetArray(string[] array, string[] result)
 {
+    int count = 0;
+    
     for (int i = 0; i < array.Length; i++)
     {
-        int count = 0;
         if (array[i].Length <= 3)
         {
             result[count] = array[i];
             count++;
-            Console.Write($"{array[i]} ");
+            // Console.Write($"{array[i]} ");
         }
     }
 
 }
 
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
 
 GetArray(array, result);
+PrintArray(result);
