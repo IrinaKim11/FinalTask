2da5e1253f3ca83970d7a9b6ee73cb3ce8dfd08c
Irina <i.kim1994@mail.ru>
2022-12-03 17:27:35 +0300

    Добавили решение задачи

---
+++ FinalTask/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] array = new string[6] { "adsdaw", "1", "DVSV", "KokkoI", "hh", "dsgshd"};
+string[] result = new string[array.Length];
+
+void GetArray(string[] array, string[] result)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int count = 0;
+        if (array[i].Length <= 3)
+        {
+            result[count] = array[i];
+            count++;
+            Console.Write($"{array[i]} ");
+        }
+    }
+
+}
+
+
+GetArray(array, result);
